d9cf4a9a1547a421c2e65b27b78f99a31dfeb48a
cndoit18 <cndoit18@outlook.com>
2024-10-25 01:06:32 +0800

    qf

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,52 @@
 vim.g.mapleader = "\\"
 
 -- Configuration
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "gy", vim.lsp.buf.type_definition)
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-vim.keymap.set("n", "gr", vim.lsp.buf.references)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, opts)
+	end,
+})
+
 vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
-vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action)
+
+-- put quickfix window always to the bottom
+local qfgroup = vim.api.nvim_create_augroup("changeQuickfix", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	group = qfgroup,
+	command = "wincmd J",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	group = qfgroup,
+	command = "setlocal wrap",
+})
+
+-- close quickfix menu after selecting choice
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "qf", "quickfix" },
+	command = [[
+	nnoremap <buffer> <CR> <CR>:cclose<CR>
+        nnoremap <buffer> k <Up><CR><C-w>p
+        nnoremap <buffer> j <Down><CR><C-w>p
+    ]],
+})
+
 vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>nf", ":Neotree focus<CR>")
 
