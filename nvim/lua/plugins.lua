45288374ba7b14065e485625e4d0506da0279ef3
cndoit18 <cndoit18@outlook.com>
2024-10-26 02:52:11 +0800

    feat(lspconfig): support venv
    Signed-off-by: cndoit18 <cndoit18@outlook.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -671,6 +671,29 @@ return {
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
+			local configs = require("lspconfig/configs")
+			local util = require("lspconfig/util")
+
+			local path = util.path
+
+			local function get_python_path(workspace)
+				-- Use activated virtualenv.
+				if vim.env.VIRTUAL_ENV then
+					return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+				end
+
+				-- Find and use virtualenv in workspace directory.
+				for _, pattern in ipairs({ "*", ".*" }) do
+					local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+					if match ~= "" then
+						return path.join(path.dirname(match), "bin", "python")
+					end
+				end
+
+				-- Fallback to system Python.
+				return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+			end
+
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			capabilities.textDocument.foldingRange = {
 				dynamicRegistration = false,
@@ -678,6 +701,11 @@ return {
 			}
 			for _, lsp in ipairs(lsp_servers) do
 				lspconfig[lsp].setup({
+					before_init = function(_, config)
+						if lsp == "pyright" then
+							config.settings.python.pythonPath = get_python_path(config.root_dir)
+						end
+					end,
 					capabilities = capabilities,
 				})
 			end
