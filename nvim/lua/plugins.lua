02c2a4fd7d6e80b1a2d037901e6269336f2708b2
cndoit18 <cndoit18@outlook.com>
2024-10-28 11:23:14 +0800

    update markdown-preview
    Change-Id: Ica8ede27db922b9c170f9092beb338c36067a3d5

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -767,9 +767,7 @@ return {
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		build = "cd app && yarn install",
 	},
 
 	{
