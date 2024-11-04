b2db9655f0ac0cc775b3d2719b236ec624735a04
cndoit18 <cndoit18@outlook.com>
2024-11-04 18:58:32 +0800

    add Rg command
    Change-Id: I429ded4dd6dda66fca3ab9c968bdc31baaddea0c

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -759,6 +759,14 @@ return {
 		"ibhagwan/fzf-lua",
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		init = function()
+			vim.api.nvim_create_user_command("Rg", function(opts)
+				require("fzf-lua").live_grep({ search = opts.args })
+			end, {
+				nargs = "?",
+				desc = "Grep for text in files.",
+			})
+		end,
 		config = function()
 			-- calling `setup` is optional for customization
 			require("fzf-lua").setup({})
