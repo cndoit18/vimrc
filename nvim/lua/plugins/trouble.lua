900ef8b0c4777a800fc1678a324240c5d352393f
cndoit18 <cndoit18@outlook.com>
2024-11-07 11:30:25 +0800

    trouble
    Change-Id: I17b9444ce2b63adfbbb8b7781a6bddd48528425b

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,63 @@
+return {
+	"folke/trouble.nvim",
+	cmd = { "Trouble" },
+	init = function()
+		vim.diagnostic.config({ virtual_text = false })
+	end,
+	opts = {
+		auto_preview = false,
+		modes = {
+			preview_diagnostics = {
+				mode = "diagnostics",
+				preview = {
+					type = "split",
+					relative = "win",
+					position = "right",
+					size = 0.3,
+				},
+			},
+		},
+	},
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+		{ "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+		{
+			"<leader>cS",
+			"<cmd>Trouble lsp toggle<cr>",
+			desc = "LSP references/definitions/... (Trouble)",
+		},
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+		{
+			"[q",
+			function()
+				if require("trouble").is_open() then
+					---@diagnostic disable-next-line: missing-parameter,missing-fields
+					require("trouble").prev({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cprev)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Previous Trouble/Quickfix Item",
+		},
+		{
+			"]q",
+			function()
+				if require("trouble").is_open() then
+					---@diagnostic disable-next-line: missing-parameter,missing-fields
+					require("trouble").next({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cnext)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Next Trouble/Quickfix Item",
+		},
+	},
+}
