0a713771808ed65208c341a80ddc790ddc9db2d3
cndoit18 <cndoit18@outlook.com>
2024-11-07 13:37:53 +0800

    add translate
    Change-Id: I26923eceee1f91b4d1d38dd5b456244407bd9fad

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,27 +58,4 @@ vim.o.cursorline = true
 vim.o.list = true
 vim.o.listchars = [[tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%]]
 
-local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
-
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-	pattern = "*",
-	group = augroup,
-	callback = function()
-		if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
-			vim.opt.relativenumber = true
-		end
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
-	pattern = "*",
-	group = augroup,
-	callback = function()
-		if vim.o.nu then
-			vim.opt.relativenumber = false
-			vim.cmd("redraw")
-		end
-	end,
-})
-
 require("config.lazy")
--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -10,31 +10,32 @@ return {
 		vim.api.nvim_create_autocmd("ColorScheme", {
 			group = grpid,
 			pattern = "gruvbox-material",
-			command = "hi NvimTreeNormal                     guibg=#181818 |"
-				.. "hi NvimTreeEndOfBuffer                guibg=#181818 |"
-				.. "hi NoiceCmdlinePopupBorderCmdline     guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePromptBorder              guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePromptNormal              guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePromptTitle               guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePromptPrefix              guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePromptCounter             guifg=#ea6962 guibg=#282828 |"
-				.. "hi TelescopePreviewTitle              guifg=#89b482 guibg=#282828 |"
-				.. "hi TelescopePreviewBorder             guifg=#89b482 guibg=#282828 |"
-				.. "hi TelescopeResultsTitle              guifg=#89b482 guibg=#282828 |"
-				.. "hi TelescopeResultsBorder             guifg=#89b482 guibg=#282828 |"
-				.. "hi TelescopeMatching                  guifg=#d8a657 guibg=#282828 |"
-				.. "hi TelescopeSelection                 guifg=#ffffff guibg=#32302f |"
-				.. "hi FloatBorder                        guifg=#ea6962 guibg=#282828 |"
-				.. "hi BqfPreviewBorder                   guifg=#ea6962 guibg=#282828 |"
-				.. "hi NormalFloat                        guibg=#282828 |"
-				.. "hi IndentBlanklineContextChar         guifg=#d3869b |"
-				.. "hi StatusColumnBorder                 guifg=#232323 |"
-				.. "hi StatusColumnBuffer                 guibg=#282828 |"
-				.. "hi CursorLineNr                       guifg=#d8a657 |"
-				.. "hi CodewindowBorder                   guifg=#ea6962 |"
-				.. "hi TabLine                            guibg=#282828 |"
+			command = "hi CursorLineNr                       guifg=#d8a657 |"
 				.. "hi LineNrAbove                        guifg=#765c3d |"
 				.. "hi LineNrBelow                        guifg=#88481e |",
+		})
+
+		local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+			pattern = "*",
+			group = augroup,
+			callback = function()
+				if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+					vim.opt.relativenumber = true
+				end
+			end,
+		})
+
+		vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+			pattern = "*",
+			group = augroup,
+			callback = function()
+				if vim.o.nu then
+					vim.opt.relativenumber = false
+					vim.cmd("redraw")
+				end
+			end,
 		})
 
 		-- Optionally configure and load the colorscheme
